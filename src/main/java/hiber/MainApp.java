53a6fdc4be959cb2f74574214f916e4b98dc1d17
Kristina <kitty_kris@bk.ru>
2021-12-28 18:11:50 +0300

    change Main - use method toString in Collection

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -27,11 +27,7 @@ public class MainApp {
 
       List<User> users = userService.listUsers();
       for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println("Car = " + user.getCar());
+         System.out.println(user.toString());
          System.out.println();
       }
 
